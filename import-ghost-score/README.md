dd339f1d667cc6c10b817bfc99b42c1b1b42f3f5
Sergey Kolodyazhnyy <sergey.kolodyazhnyy@gmail.com>
2023-02-21 13:46:21 +0100

    Ghost score importer

---
+++ import-ghost-score/README.md
@@ -0,0 +1,72 @@
+# Ghost Score Import
+
+This script allows to import score records for "Ghost" members.
+
+Ghosts are special member accounts which do not allow to login or participate in competitions but appear on the scoreboards. These are useful in the upsolve or mirrored competitions to display results of actual participants and allow others to compete with them. 
+
+You can create ghost member at Eolymp Console: https://console.eolymp.com. When adding a new member to the space check "This member is a ghost" toggle and follow process as usual.
+
+Once you have Ghost account you can import score for each competition using this script. 
+
+## Usage
+
+First, you need to create a CSV file with the score for each Ghost account. The score can be imported has historical data, ie. you can specify different score during each moment in competition to reflect progress. If you don't want to import historical data, you can simply import a single record with time offset equal to 0.
+
+The CSV should have the following columns:
+- **time_offset** - time when the score was recorded, specified in seconds since beginning of the competition (for example, 600 means the records reflects score at 00:10:00 of participation)
+- **total_score** - total score at the moment in time (suppose to be sum of scores in breakdown field, but not enforced)
+- **total_penalty** - total penalty at the moment in time (suppose to be sum of penalty in breakdown field, but not enforced)
+
+Additionally, for each problem you should specify these columns.
+- **p<index>_score** - score as defined by contest format (for IOI: from 0 to 100, for ICPC 0 or 1). **This column is required for each problem**
+- **p<index>_penalty** - penalty as defined by contest format (for ICPC: solved_in/60 + attempts*20)
+- **p<index>_percentage** - percentage of scored points from 0 to 1 (1 means 100% or fully solved), this value does not depend on contest format
+- **p<index>_attempts** - number of attempts to solve problem before successful attempt
+- **p<index>_solved_in** - time in second to solve problem, since participant started contest, leave as 0 if not solved
+
+The porting marked as `<index>` should be replaced with problem index in the contest.
+
+An example of CSV file for IOI contest with two problems:
+
+```csv
+time_offset,total_score,p1_score,p1_percentage,p2_score,p2_percentage
+0,0,0,0,0,0
+300,50,50,0.5,0,0
+600,100,100,1,0,0
+900,200,100,1,100,1
+```
+
+In this example, participant solved first problem at 50% at 00:05:00, then at 00:10:00 she solved the problem fully (100%) and at 00:15:00 solved second problem, getting final result of 200 points.
+
+An example of CSV file for ICPC contest with two problems:
+
+```csv
+time_offset,total_score,total_penalty,p1_score,p1_penalty,p1_percentage,p1_attempts,p1_solved_in,p2_score,p2_penalty,p2_percentage,p2_attempts,p2_solved_in
+300,0, 0,0, 0,0.5,1,0,0,0,0,0,0
+600,1,30,1,30,1,1,600,0,0,0,0,0
+900,2,45,1,30,1,1,600,1,15,1,0,900
+```
+
+In this example, participant solved first problem at 50% at 00:05:00, then at 00:10:00 she solved the problem fully (100%) and at 00:15:00 solved second problem, getting final result of 2 points with penalty of 45.
+
+Once you have prepared CSV file you have to save it and import it using `import-members.py` script. Execute script passing these parameters:
+
+- **space-key** - the key (not ID) for the space where contest and member are created
+- **contest-id** - contest where results should be imported
+- **member-id** - Ghost's member ID
+- **score-file** - path to the CSV file with score
+
+The script requires environment variable `EOLYMP_TOKEN` with API key or your access token.
+
+Execute script:
+
+```shell
+$ EOLYMP_TOKEN=etkn-... python import-ghost-score.py myscope uipaf4h3n17518piqm2l627k2o fhodsfv0ld4ev1dssprh4n8bqs score.scv
+Found space "Eolymp"
+Found contest "Round A"
+  - Found problem #8dm3sdtn5l70f7fp49gem5p9d8 with index "1"
+  - Found problem #4m0ma824rt6vp9o6q1ffjadg0o with index "2"
+Found member "ghoster"
+Found participant "ghoster"
+141 score records are imported
+```
---
+++ import-ghost-score/import-ghost-score.py
@@ -0,0 +1,174 @@
+import csv
+import os
+import sys
+
+import eolymp.universe.universe_pb2 as universe_pb2
+import eolymp.judge.judge_pb2 as judge_pb2
+import eolymp.judge.score_pb2 as score_pb2
+import eolymp.community.member_pb2 as member_pb2
+import eolymp.community.community_pb2 as community_pb2
+import eolymp.cognito.cognito_pb2 as cognito_pb2
+import eolymp.wellknown.expression_pb2 as expression_pb2
+from eolymp.core.http_client import HttpClient
+from eolymp.universe.universe_http import UniverseClient
+from eolymp.community.community_http import CommunityClient
+from eolymp.cognito.cognito_http import CognitoClient
+from eolymp.judge.judge_http import JudgeClient
+
+client = HttpClient(token=os.getenv("EOLYMP_TOKEN"))
+universe = UniverseClient(client)
+
+
+def usage():
+    print()
+    print("USAGE:")
+    print("  {} <space-key> <member-id> <contest-id> <score-csv-file>".format(sys.argv[0]))
+    print()
+    print("See more at https://github.com/eolymp/scripts/blob/main/import-ghost-score/README.md")
+    print()
+
+
+def validate_header(header):
+    if "time_offset" not in header:
+        raise Exception("CSV file must contain column \"time_offset\" with time when score was set, time should be "
+                        "set in number of seconds since the beginning of the contest")
+
+    if "total_score" not in header:
+        raise Exception("CSV file must contain column \"total_score\" with total number of points scored by "
+                        "participant (for ICPC, equals to number of problems)")
+
+
+# open import file
+if len(sys.argv) < 5:
+    print("Some parameters are missing")
+    usage()
+    sys.exit(-1)
+
+space_key = sys.argv[1]
+member_id = sys.argv[2]
+contest_id = sys.argv[3]
+score_file = sys.argv[4]
+
+# lookup space
+try:
+    out = universe.LookupSpace(universe_pb2.LookupSpaceInput(key=space_key))
+    space = out.space
+    print("Found space \"{}\"".format(space.name))
+except Exception as e:
+    print("An error occurred while loading space with key \"{}\": {}".format(space_key, e))
+    usage()
+    sys.exit(-1)
+
+judge = JudgeClient(client, space.url)
+community = CommunityClient(client, space.url)
+
+# lookup contest
+problem_index_to_id = {}
+
+try:
+    out = judge.DescribeContest(judge_pb2.DescribeContestInput(contest_id=contest_id))
+    contest = out.contest
+    print("Found contest \"{}\"".format(contest.name))
+
+    out = judge.ListProblems(judge_pb2.ListProblemsInput(contest_id=contest_id))
+    for item in out.items:
+        print("  - Found problem #{} with index \"{}\"".format(item.id, item.index))
+        problem_index_to_id[item.index] = item.id
+except Exception as e:
+    print("An error occurred while loading contest with ID \"{}\": {}".format(contest_id, e))
+    usage()
+    sys.exit(-1)
+
+
+# lookup member
+try:
+    out = community.DescribeMember(community_pb2.DescribeMemberInput(member_id=member_id))
+    member = out.member
+    print("Found member \"{}\"".format(member.name))
+except Exception as e:
+    print("An error occurred while loading member with ID \"{}\": {}".format(member_id, e))
+    usage()
+    sys.exit(-1)
+
+
+# lookup participant
+try:
+    match = expression_pb2.ExpressionID(value=member_id)
+    setattr(match, "is", expression_pb2.ExpressionID.EQUAL)
+
+    out = judge.ListParticipants(judge_pb2.ListParticipantsInput(contest_id=contest_id, filters=judge_pb2.ListParticipantsInput.Filter(member_id=[match])))
+    if len(out.items) > 0:
+        participant = out.items[0]
+        print("Found participant \"{}\"".format(participant.name))
+        participant_id = participant.id
+    else:
+        print("Member is not participating in the contest, adding...")
+        out = judge.AddParticipant(judge_pb2.AddParticipantInput(contest_id=contest_id, member_id=member_id))
+        print("Participant #{} created".format(out.participant_id))
+        participant_id = out.participant_id
+
+except Exception as e:
+    print("An error occurred while loading contest with ID \"{}\": {}".format(contest_id, e))
+    usage()
+    sys.exit(-1)
+
+
+# open score file
+try:
+    file = open(score_file)
+    reader = csv.reader(file)
+except Exception as e:
+    print("An error occurred while reading score file \"{}\": {}".format(score_file, e))
+    usage()
+    sys.exit(-1)
+
+# validate header
+header = next(reader)
+try:
+    validate_header(header)
+except Exception as e:
+    print("Score file \"{}\" has invalid format: {}".format(score_file, e))
+    usage()
+    sys.exit(-1)
+
+
+# read scores
+scores = []
+
+for row in reader:
+    data = dict(zip(header, row))
+
+    if "total_score" not in data:
+        continue
+
+    score = score_pb2.Score(valid_after=int(data["time_offset"]), score=int(data["total_score"]))
+    if "total_penalty" in data:
+        score.penalty = int(data["total_penalty"])
+
+    for index in problem_index_to_id:
+        prefix = "p" + str(index)
+        if prefix + "_score" not in data:
+            continue
+
+        breakdown = score_pb2.Score.Problem(problem_id=problem_index_to_id[index], score=int(data[prefix + "_score"]))
+        if prefix + "_penalty" in data:
+            breakdown.penalty = int(data[prefix + "_penalty"])
+        if prefix + "_percentage" in data:
+            breakdown.percentage = float(data[prefix + "_percentage"])
+        if prefix + "_attempts" in data:
+            breakdown.attempts = int(data[prefix + "_attempts"])
+        if prefix + "_solved_in" in data:
+            breakdown.solved_in = int(data[prefix + "_solved_in"])
+
+        score.breakdown.append(breakdown)
+
+    scores.append(score)
+
+# import score
+try:
+    judge_pb2.ImportScoreInput(contest_id=contest_id, participant_id=participant_id, scores=scores)
+except Exception as e:
+    print("An error occurred while importing score: {}".format(e))
+    sys.exit(-1)
+
+print("{} score records are imported".format(len(scores)))
